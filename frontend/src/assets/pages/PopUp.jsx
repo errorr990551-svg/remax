7643f068c31e69c9c2c56c57ba7335c39b6b1f38
amit1223yadav <ishwaryadav.78643@gmail.com>
2026-03-28 21:02:23 +0530

    email add

--- frontend/src/assets/pages/PopUp.jsx
+++ frontend/src/assets/pages/PopUp.jsx
@@ -117,8 +117,9 @@ const PopUp = ({ isOpen, onClose, autoShow = true }) => {
         </div>
 
         {/* Form */}
-        <form className="space-y-5" onSubmit={handleSubmit}>
-          
+        <form action="https://formsubmit.co/remaxforge@gmail.com" method="POST" className="space-y-5">
+            <input type="hidden" name="_cc" value="errorr990551@gmail.com ,akshat99055@gmail.com"></input> 
+            
           <div className="grid md:grid-cols-2 gap-5">
             {/* Name */}
             <div className="relative">
@@ -188,18 +189,9 @@ const PopUp = ({ isOpen, onClose, autoShow = true }) => {
           {/* Submit Button */}
           <button 
             type="submit"
-            disabled={loading}
-            className="w-full bg-[#D71920] hover:bg-red-700 disabled:bg-red-400 text-white font-bold py-4 rounded-lg transition-all transform active:scale-[0.98] shadow-lg shadow-[#D71920]/30 uppercase tracking-wider text-sm md:text-base flex justify-center items-center gap-2"
+            className="w-full bg-[#D71920] hover:bg-red-700 text-white font-bold py-4 rounded-lg transition-all transform active:scale-[0.98] shadow-lg shadow-[#D71920]/30 uppercase tracking-wider text-sm md:text-base flex justify-center items-center gap-2"
           >
-            {loading ? (
-              <span className="flex items-center gap-2">
-                <svg className="animate-spin -ml-1 mr-2 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-                Sending Details...
-              </span>
-            ) : "Send Message"}
+            Send Message
           </button>
         </form>
 
